e1fb88ac44af53cac9e3e1d3c545d5a46165ff77
“juarez” <juareznasato@gmail.com>
2019-05-11 22:56:42 -0300

    script npm publish.

--- npm-pub.sh
+++ npm-pub.sh
@@ -12,10 +12,11 @@
 #        		                                                     #
 ######################################################################
 #
-# --> COMANDOS:
-# $ npm login
+# --> COMANDOS <--
+# $ npm login               logar no npm
 # $ sh npm-pub.sh -pk       pack
 # $ sh npm-pub.sh -pb       publish
+#
 echo "\033[1;36m  #    # #####  #    #       #####  #    # #####  \033[0m";
 echo "\033[1;36m  ##   # #    # ##  ##       #    # #    # #    # \033[0m";
 echo "\033[1;36m  # #  # #    # # ## #       #    # #    # #    # \033[0m";
@@ -24,21 +25,23 @@ echo "\033[1;36m  #   ## #      #    #       #      #    # #    # \033[0m";
 echo "\033[1;36m  #    # #      #    #       #      #    # #    # \033[0m";
 echo "\033[1;36m  #    # #      #    #       #      ###### #####  \033[0m";
 echo
-echo "Iníciando cópia de arquivos e publicação NPM.";
-echo
+echo "Copiando arquivos...";
 cp package.json npm;
 cp README.md npm;
 cp src/components/VuetifyMoney.vue npm;
+echo "Copiado.";
 if [ $1 = "-pk" ];
 then
-    echo "Empacotando...";
+    echo "Empacotando arquivos...";
     npm pack ./npm;
+    echo "Empacotado.";
 fi;
 
 if [ $1 = "-pb" ];
 then
-    echo "Publicando...";
+    echo "Publicando no npm...";
     npm publish ./npm;
+    echo "Publicado."
 fi;
 echo "Fim.";
 
